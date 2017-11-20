726199e10b9314737a912d06ac928796c8fc289c
Paweł Zimnowodzki <pawel@prowebsoft.pl>
2017-11-20 19:03:09 +0100

    first test for TicTacToe game

--- test/TestTest.php
+++ test/TestTest.php
@@ -1,10 +1,19 @@
 <?php
 declare(strict_types=1);
-
 namespace TicTacToeTest;
 
+include dirname(__DIR__) . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
 
-class TestTest
+use PHPUnit\Framework\TestCase;
+
+class TestTest extends TestCase
 {
-
+    /**
+     * @test
+     */
+    public function self()
+    {
+        $game = new TicTacToe\Test();
+        self::assertTrue(is_object($game));
+    }
 }
