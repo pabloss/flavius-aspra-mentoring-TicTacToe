8b14b59800280082137450a341b602ed22656540
Paweł Zimnowodzki <pawel.zimnowodzki@gmail.com>
2017-11-28 10:58:38 +0100

    unit GameTest correction

--- tests/src/TicTacToe/GameTest.php
+++ tests/src/TicTacToe/GameTest.php
@@ -31,7 +31,7 @@ class GameTest extends TestCase
         $player0->takeTile(new \TicTacToe\Tile(0, 1));
         $playerX->takeTile(new \TicTacToe\Tile(1, 0));
         $player0->takeTile(new \TicTacToe\Tile(1, 1));
-        self::assertEquals(['X', ' ', ' ', ' ', '0', 'X', ' ', '0', ' '], $game->board());
+        self::assertEquals(['X', '0', ' ', 'X', '0', ' ', ' ', ' ', ' '], $game->board());
         self::assertEquals([[0, 0], [0, 1], [1, 0], [1, 1]], $game->history());
     }
 }
