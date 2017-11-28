4a29b66b3729648d8047affa1c6106fed5de41c7
Paweł Zimnowodzki <pawel.zimnowodzki@gmail.com>
2017-11-28 10:42:18 +0100

    making TileTest pass

--- src/Tile.php
+++ src/Tile.php
@@ -5,4 +5,22 @@ namespace TicTacToe;
 
 class Tile
 {
+    private $row;
+    private $column;
+
+    public function __construct($row, $column)
+    {
+        $this->row = $row;
+        $this->column = $column;
+    }
+
+    public function row()
+    {
+        return $this->row;
+    }
+
+    public function column()
+    {
+        return $this->column;
+    }
 }
