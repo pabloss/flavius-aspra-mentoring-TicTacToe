b961f700c68ae263942917d663276db05c0203f2
Paweł Zimnowodzki <pawel.zimnowodzki@gmail.com>
2017-11-28 09:21:02 +0100

    players shuld not repeat theirs test at one turn

--- tests/src/TicTacToe/GameTest.php
+++ tests/src/TicTacToe/GameTest.php
@@ -19,4 +19,18 @@ class GameTest extends TestCase
         self::assertInstanceOf('TicTacToe\Player', $playerX);
         self::assertInstanceOf('TicTacToe\Player', $player0);
     }
+    
+    /**
+     * @test
+     */
+    public function game_should_record_correct_turns()
+    {
+        $game = new TicTacToe();
+        list($playerX, $player0) = $game->players('X', '0');
+        $playerX->takeTile(new \TicTacToe\Tile(0, 0));
+        $player0->takeTile(new \TicTacToe\Tile(0, 1));
+        $playerX->takeTile(new \TicTacToe\Tile(1, 0));
+        $player0->takeTile(new \TicTacToe\Tile(1, 1));
+        self::assertEquals(['X', ' ', ' ', ' ', '0', 'X', ' ', '0', ' '], $game->board());
+    }
 }
