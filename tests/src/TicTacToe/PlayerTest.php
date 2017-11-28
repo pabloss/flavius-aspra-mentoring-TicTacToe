62650121fb331e5c312920fb2252fe03a9551fcd
Paweł Zimnowodzki <pawel.zimnowodzki@gmail.com>
2017-11-28 10:10:57 +0100

    refactoring unit test of Player

--- tests/src/TicTacToe/PlayerTest.php
+++ tests/src/TicTacToe/PlayerTest.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 namespace TicTacToeTest\src\TicTacToe;
 
 use PHPUnit\Framework\TestCase;
+use TicTacToe\Game;
 use TicTacToe\Player;
 
 class PlayerTest extends TestCase
@@ -13,7 +14,7 @@ class PlayerTest extends TestCase
      */
     public function player_has_symbol()
     {
-        $player = new Player('X');
+        $player = new Player('X', new Game());
         self::assertEquals('X', $player->symbol());
     }
 }
