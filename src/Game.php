78a0faa45b5c3f1e5a16540b203975ebbb10ca72
Paweł Zimnowodzki <pawel.zimnowodzki@gmail.com>
2017-11-28 07:46:46 +0100

    making test of players duplication pass

--- src/Game.php
+++ src/Game.php
@@ -3,10 +3,15 @@ declare(strict_types=1);
 
 namespace TicTacToe;
 
+use TicTacToe\Exception\DuplicatePlayersException;
+
 class Game
 {
     public function players($symbolX, $symbol0)
     {
+        if ($symbolX === $symbol0) {
+            throw new DuplicatePlayersException();
+        }
         return [new Player($symbolX), new Player($symbol0)];
     }
 }
