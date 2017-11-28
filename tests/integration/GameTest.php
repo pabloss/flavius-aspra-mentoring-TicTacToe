c648e97c790c2ed2bcade13e6b11e13aea99009a
Paweł Zimnowodzki <pawel.zimnowodzki@gmail.com>
2017-11-28 07:48:59 +0100

    integration test to prevent turn duplication

--- tests/integration/GameTest.php
+++ tests/integration/GameTest.php
@@ -29,4 +29,16 @@ class GameTest extends TestCase
         $game = new TicTacToe();
         $game->players('X', 'X');
     }
+
+    /**
+     * @test
+     * @expectedException TicTacToe\Exception\DuplicateTurnsException
+     */
+    public function players_take_turns()
+    {
+        $game = new \TicTacToe\Game();
+        list($playerX, $player0) = $game->players('X', '0');
+        $playerX->takeTile(new \TicTacToe\Tile(0, 0));
+        $playerX->takeTile(new \TicTacToe\Tile(1, 1));
+    }
 }
