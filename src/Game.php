975da71484813d79fdab1e3473dc122143c48f1a
Paweł Zimnowodzki <pawel.zimnowodzki@gmail.com>
2017-11-28 11:00:05 +0100

    make unit test pass

--- src/Game.php
+++ src/Game.php
@@ -7,11 +7,36 @@ use TicTacToe\Exception\DuplicatePlayersException;
 
 class Game
 {
+    private $board;
+    private $history;
+
+    public function __construct()
+    {
+        $this->board = \array_fill(0, 9, ' ');
+        $this->history = [];
+    }
+
     public function players($symbolX, $symbol0)
     {
         if ($symbolX === $symbol0) {
             throw new DuplicatePlayersException();
         }
-        return [new Player($symbolX), new Player($symbol0)];
+        return [new Player($symbolX, $this), new Player($symbol0, $this)];
+    }
+
+    public function board()
+    {
+        return $this->board;
+    }
+
+    public function takeFile(Tile $tile, Player $player)
+    {
+        $this->board[$tile->column() + 3*$tile->row()] = $player->symbol();
+        $this->history[\count($this->history) % 9] = [$tile->row(), $tile->column()];
+    }
+
+    public function history()
+    {
+        return $this->history;
     }
 }
--- src/Player.php
+++ src/Player.php
@@ -6,15 +6,22 @@ namespace TicTacToe;
 class Player
 {
     private $symbol;
+    private $game;
 
-    public function __construct($symbol)
+    public function __construct($symbol, $game)
     {
         $this->symbol = $symbol;
+        $this->game = $game;
     }
 
 
     public function symbol()
     {
         return $this->symbol;
+    }
+
+    public function takeTile(Tile $tile)
+    {
+        $this->game->takeFile($tile, $this);
     }
 }
