37b4915e1d9202c4b81ed7730f871db197fd6d01
Paweł Zimnowodzki <pawel.zimnowodzki@gmail.com>
2017-12-10 20:42:47 +0100

    a new acceptance test for choosing a Tile by AI

--- tests/acceptance/PlayingAgainstAISimulationTest.php
+++ tests/acceptance/PlayingAgainstAISimulationTest.php
@@ -4,11 +4,13 @@ declare(strict_types=1);
 namespace TicTacToeTest\acceptance;
 
 use PHPUnit\Framework\TestCase;
+use TicTacToe\AI\AI;
 use TicTacToe\AI\AIPlayer;
 use TicTacToe\Game as TicTacToe;
 use TicTacToe\Player;
 use TicTacToe\Symbol;
 use TicTacToe\Tile;
+use TicTacToe\Type\PlayerType as Type;
 
 class PlayingAgainstAISimulationTest extends TestCase
 {
@@ -18,12 +20,11 @@ class PlayingAgainstAISimulationTest extends TestCase
     public function random_looped_taken_tilles_should_fill_whole_board()
     {
         $game = new TicTacToe();
-        $factory = new \TicTacToe\Factory\PlayerFactory();
-        $playerX = $factory->createAI('X');
-        $player0 = $factory->createReal('0');
+        list($playerX, $player0) = $game->players(new Symbol('X'), new Symbol('0'));
+        $ai = new AI();
         for ($i = 0; $i < 9; $i++) {
-            $playerX->takeTile();
-            $player0->takeTile($this->findNextFreeTile($game));
+            $playerX->takeTile($ai->chooseFreeTile());
+            $player0->takeTile($ai->chooseFreeTile());
         }
         self::assertTrue(
             $game->winner()->symbol()->value() === 'X' ||
