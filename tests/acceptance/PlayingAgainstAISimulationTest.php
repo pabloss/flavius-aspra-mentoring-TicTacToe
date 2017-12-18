b705600dec665387a5f50f14d96923191c527c9a
Paweł Zimnowodzki <pawel.zimnowodzki@gmail.com>
2017-12-18 21:34:27 +0100

    removing game visualisation

--- tests/acceptance/PlayingAgainstAISimulationTest.php
+++ tests/acceptance/PlayingAgainstAISimulationTest.php
@@ -41,42 +41,11 @@ class PlayingAgainstAISimulationTest extends TestCase
             $carry = $carry || (\is_null($value) === false);
             return $carry;
         }, false));
-        $this->visualise_board($game);
     }
 
     private function simulate_choosing_tiles_of_real_player()
     {
         $ai = new AI($this->game);
         return $ai->takeRandomFreeTile();
-    }
-
-    private function visualise_board(TicTacToe $game)
-    {
-        $board = $game->board();
-
-        if (\is_null($game->winner())) {
-            \print_r("\nThere IS NO winner.\n");
-        } else {
-            \print_r("\nThe winner IS '" .
-            $game->winner()->symbol()->value() .
-            "' player.\n");
-        }
-
-        \print_r("\nThe board has " . \count($board). " Tiles.\n\n");
-        \print_r("Here is how the board looks like at the end of game below:\n");
-        \print_r("----------------------------------------------------------\n");
-        foreach ($board as $i => $value) {
-            if ($i % 3 ===0) {
-                \print_r("\n");
-            }
-            \print_r("|");
-            if (!empty($value)) {
-                \print_r($value->symbol()->value());
-            } else {
-                \print_r("_");
-            }
-            \print_r("|");
-        }
-        \print_r("\n\n----------------------------------------------------------\n");
     }
 }
